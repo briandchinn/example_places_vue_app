e962077def12d999abb3db9839c4c720674a278f
briandchinn <bchinn310@gmail.com>
2019-06-19 07:23:47 -0700

    adds example places front end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div v-for="place in places">
+      <h3>Name: {{ place.name }}</h3>
+      <h4>Address: {{ place.address }}</h4>
+      <hr>
+    </div>
+    
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Places App",
+      places: [],
+      currentRecipe: {},
+      errors: []
+    };
+  },
+  created: function() {
+    axios.get("/api/places").then(response => {
+      this.places = response.data;
+      console.log(this.places);
+    });
+  },
+  methods: {
+    showRecipe: function(recipe) {
+      if (this.currentRecipe === recipe) {
+        this.currentRecipe = null;
+      } else {
+        this.currentRecipe = recipe;
+      }
+    },
+    destroyRecipe: function(recipe) {
+      // send an axios delete request to the backend to remove recipe from database
+      axios.delete("/api/recipes/" + recipe.id).then(response => {
+        console.log("Success!", response.data);
+        // find index of recipe in recipes array
+        var index = this.recipes.indexOf(recipe);
+        // splice recipes array at index
+        this.recipes.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
